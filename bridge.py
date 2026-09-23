3e72f05ecc5211e83c4525954565c1b06681648e
Derek_Yeadoer_Dong <150316766+yeadoer001@users.noreply.github.com>
2026-09-23 16:28:59 +0800

    1.1

--- bridge.py
+++ bridge.py
@@ -6,7 +6,7 @@ from bdf2rad.core.converter import convert
 
 # ================= USER CONFIGURATION =================
 INPUT_BDF = r'E:\openradioss\input\01_GRID_basic.bdf'
-OUTPUT_DIR = r'E:\openradioss\output'
+OUTPUT_DIR = r'D:\BDF2RAD-X\bdf2rad\0-output'
 BLOCK_ROOT = Path(__file__).resolve().parent / 'blocks'
 ENCODING = 'gb18030'
 # ======================================================
